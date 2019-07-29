a2c5b7453f2928a45af696c148cedc21d20f2846
Raut <vraut@internal.tavisca.com>
2019-07-29 13:51:24 +0530

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ConsoleClient
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            IPAddress ip = IPAddress.Parse("172.16.5.199");
+            int port = 5000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+
+            
+            Console.WriteLine("Connected to the Chat Room. Type and hit Enter to broadcast your message.\n");
+            NetworkStream ns = client.GetStream();
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+            thread.Start(client);
+
+            string s;
+            while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(s);
+                ns.Write(buffer, 0, buffer.Length);
+            }
+
+            client.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            client.Close();
+            Console.WriteLine("disconnect from server!!");
+            Console.ReadKey();
+        }
+
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+            }
+        }
+    }
+}
+
